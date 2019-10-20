a551cbef3c39394d0594f6dce8453dc85cf10433
Alexandre Felix Souza <felix.bnu@outlook.com>
2019-10-20 13:10:32 -0700

    Projeto de histórico

---
+++ HistoryService/src/test/java/spring/microservices/historySearch/HistoryService/HistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.microservices.historySearch.HistoryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
